e50f63c9bbedd3e7b39eac706eb49883d1089ff7
Centennnial <71433912+Centennnial@users.noreply.github.com>
2020-10-30 23:26:04 +0530

    Code setup, Fixed routing, And missing code, mongoDB connection

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # COMP229-F2020-MidTerm-301106486
+
+## Steps
+    npm install
+    npm start
---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  "URI": " mongodb+srv://gargi:gargi@123@cluster0.ug1pv.mongodb.net/books299?retryWrites=true&w=majority"
+};
+
+
+// COMP229-F2020-Midterm-301106486
